dfc94e62df77e5b1eb052d9649b4f113fd5d27a0
a88csleader-cmd <a88csleader@gmail.com>
2026-03-12 17:14:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = "https://script.google.com/macros/s/AKfycbwtOQMmrUPCP5KW7p8528rIEudqVj17nr8DLfvQqbYLufbtlDc2jbE7uIY0XALy64Vl6g/exec"; // เปลี่ยนเป็น URL ล่าสุดของคุณ
+const API = "https://script.google.com/macros/s/AKfycbyXs5Nahf2HqJpiBzh2cXkG-vtcI0Puh7Do3utePtM6HAlDDoD_UqEsvNkzI0hXE4fN3w/exec"; // เปลี่ยนเป็น URL ล่าสุดของคุณ
 const LINE_LINK = "https://lin.ee/Nb2TD8R";
 
 document.getElementById("pg-start-btn").addEventListener("click", playGame);
